26ea543d1c13360bb47963501eda6c254ee463ef
4mad <mydidig@gmail.com>
2017-03-16 17:49:26 -0400

    Initial push
    LWJGL files are synced via seperate program.

---
+++ src/starter/RenderGeom.java
@@ -0,0 +1,12 @@
+//David Govorko, 3/16/2017
+package starter;
+
+import org.lwjgl.opengl.GL11;
+//This class just does the rendering geometry, no colors or fancy effects.
+//Currently Circle, line, point, rectangle, and convex Polygons are supported.
+
+public class RenderGeom {
+
+	
+	
+}
